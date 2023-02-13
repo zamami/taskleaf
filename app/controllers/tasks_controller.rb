86634770e9b687a61c27190c5be3be372f889d05
zamami <s1413115@gmail.com>
2023-02-13 12:04:36 +0900

    create update func

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice:"タスク「#{task.name}」を更新しました。"
   end
 
   private
