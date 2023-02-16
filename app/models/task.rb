6ae8e2011a7bc3cd0167726ec24badeb07c0689e
zamami <s1413115@gmail.com>
2023-02-17 08:08:09 +0900

    create admin to usertable

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,20 @@
 class Task < ApplicationRecord
+    before_validation :set_nameless_name
+    validates :name, presence: true
+    validates :name, length: { maximum:30 }
+    validate :validate_name_not_including_comma
+
+
+
+
+    private
+
+    def validate_name_not_including_comma
+        errors.add(:name, 'にカンマを含めることはできません')if name&.include?(',')
+    end
+
+    def set_nameless_name
+        self.name = '名前なし' if name.blank?
+    end
+
 end
