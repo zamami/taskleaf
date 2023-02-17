b78fae63b507e15de215e6fe208a3382c339a26d
zamami <s1413115@gmail.com>
2023-02-17 21:25:50 +0900

    画像投稿の実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Asia/Tokyo'
+
 
     # Configuration for the application, engines, and railties goes here.
     #
